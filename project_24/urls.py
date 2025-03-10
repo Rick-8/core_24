dea31e294d581365fabc3eb453ea1e8332ef3a0d
Rick-8 <npopbwls@students.codeinstitute.net>
2025-03-10 13:38:43 +0000

    Add - Project_24 added

--- project_24/urls.py
+++ project_24/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for code_24 project.
+URL configuration for project_24 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
--- project_24/wsgi.py
+++ project_24/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for code_24 project.
+WSGI config for project_24 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_24.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_24.settings')
 
 application = get_wsgi_application()
